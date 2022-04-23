fbf31a09c08fd9f02015ed16f9a666095918297f
Vladislav Polezhaev <vlad.polezhaev@gmail.com>
2022-04-23 15:23:13 +0300

    homework-3

--- src/components/event/event.jsx
+++ src/components/event/event.jsx
@@ -1,42 +1,88 @@
 import React from "react"
 import { useParams } from "react-router-dom"
+import { useState } from "react"
+import { events } from "../../mocks"
 
 
-const Event = ({events}) => {
+const Event = () => {
   const {id} = useParams()
+  const evts = events.filter(evt => id === evt._id)[0]
+  const dateType = () => {
+    if (id) { 
+    return (<input
+      type="datetime-local"
+      onChange={handleFieldChange}
+      className="board__input board__input--date"
+      name="date"
+      value={new Date(form.date).toISOString().split(':', 2).join(':')}
+    />)
+    }
+    return (
+      <input
+        type="datetime-local"
+        onChange={handleFieldChange}
+        className="board__input board__input--date"
+        name="date"
+      />
+    )
+  }
+  const eventType = () => {
+    if (id) {
+      return ({
+        theme: evts.theme,
+        comment: evts.comment,
+        date: new Date(evts.date).toISOString().split(':', 2).join(':'),
+      })
+    } 
+    return ({
+      theme: '',
+      comment: '',
+      date: new Date(),
+    })
+  }
+  
+  const [form, setForm] = useState(() => eventType())
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log('form', form)
+  }
+
+  const handleFieldChange = (evt) => {
+    const {name, value} = evt.target
+    setForm({...form, [name]: value})
+  }
   return (
-    <section class="board">
-      <form class="board__form">
-          <h2 class="board__title">{id ? 'Редактирование события' : 'Добавление события'}</h2>
-          <fieldset class="board__field board__field--theme">
-            <label for="theme" class="board__label board__label--theme">Тема:</label>
+    <section className="board">
+      <form className="board__form" onSubmit={handleSubmit}>
+          <h2 className="board__title">{id ? 'Редактирование события' : 'Добавление события'}</h2>
+          <fieldset className="board__field board__field--theme">
+            <label for="theme" className="board__label board__label--theme">Тема:</label>
             <textarea
               type="text"
-              class="board__input board__input--theme"
+              onChange={handleFieldChange}
+              className="board__input board__input--theme"
               name="theme"
               required
-            ></textarea>
+            >{form.theme}</textarea>
           </fieldset>
-          <fieldset class="board__field board__field--comment">
-            <label for="comment" class="board__label board__label--comment">Комментарий:</label>
+          <fieldset className="board__field board__field--comment">
+            <label for="comment" className="board__label board__label--comment">Комментарий:</label>
             <textarea
               type="text"
-              class="board__input board__input--comment"
+              onChange={handleFieldChange}
+              className="board__input board__input--comment"
               name="comment"
               required
-            ></textarea>
+            >{form.comment}</textarea>
           </fieldset>
-          <fieldset class="board__field board__field--date">
-            <label for="date" class="board__label board__label--date">Дата:</label>
-            <input
-              type="datetime-local"
-              class="board__input board__input--date"
-              name="date"
-            />
+          <fieldset className="board__field board__field--date">
+            <label for="date" className="board__label board__label--date">Дата:</label>
+            {dateType()}
           </fieldset>
-          <div class="btns">
-            <button type="submit" class="btn-submit">Сохранить</button>
-            <button type="reset" class="btn-reset">Очистить</button>
+          <div className="btns">
+            <button type="submit" className="btn-submit">Сохранить</button>
+            <button type="reset" className="btn-reset">Очистить</button>
           </div>
       </form>
     </section>
--- src/pages/archive/archive.jsx
+++ src/pages/archive/archive.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../../components/header/header"
 import Board from "../../components/board/board"
-import { useLocation }  from "react-router-dom"
 
 const Archive = ({events}) => {
     
@@ -11,7 +10,6 @@ const Archive = ({events}) => {
     // const render = () => {
     //     return id ? `ID есть, вот он - ${id}` : 'Нет ID никакого'
     // }
-    const {pathname} = useLocation()
     const archiveEvents = events.filter(x => x.archive)
     return (
         <>
