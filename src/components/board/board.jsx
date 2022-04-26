eb766131a9b40947b0dd8bfaee9ad759c5469174
Vladislav Polezhaev <vlad.polezhaev@gmail.com>
2022-04-26 21:34:23 +0300

    homework-4

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -4,19 +4,27 @@ import Sorting from "../../components/sorting/sorting"
 import LoadMore from "../../components/load-more/load-more"
 import { AppRoute } from "../../const"
 import { useLocation } from "react-router-dom"
+import { useState } from "react"
 
 
 const Board = ({events}) => {
     const {pathname} = useLocation()
+
+    const [step, setStep] = useState(5);
+
+    const handleLoadMore = () => {
+      events.length >= step ? setStep(step + 5) : setStep(events.length);
+    }
+
     return (
         <section className="board">
             {pathname === AppRoute.MAIN && <Sorting />}
                 <div className="board__events">
-                    {events.map(event => <Card event={event} key={event._id} />)}
+                    {events.slice(0, step).map(event => <Card event={event} key={event._id} />)}
                 </div>
             {/* считаю что проверка излишняя, но вдруг условия изменятся */}
-            {pathname === AppRoute.MAIN && <LoadMore />} 
-            {pathname === AppRoute.ARCHIVE && <LoadMore />}
+            {pathname === AppRoute.MAIN && <LoadMore handleLoadMore={handleLoadMore} />} 
+            {pathname === AppRoute.ARCHIVE && <LoadMore handleLoadMore={handleLoadMore} />}
         </section>
     )
 }
